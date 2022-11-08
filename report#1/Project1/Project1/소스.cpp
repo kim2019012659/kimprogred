ca64e5c528f153dc12b66aecd3857aecde219802
kim2019012659 <als4923@naver.com>
2022-11-09 00:37:28 +0900

    report#1

--- report#1/Project1/Project1/소스.cpp
+++ report#1/Project1/Project1/소스.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+//랜덤으로 양의 정수를 생산하기 위해 난수생성
 void GenRandSeed()
 {
     srand((unsigned int)(time(NULL)));
@@ -23,14 +23,14 @@ int main(void)
     int RandomNumber = 0;
     int Answer = 0;
     int i;
-
+    //사용자가 임의의 정수를 입력합니다.
     printf("Enter the range of numbers you want to match... : ");
     scanf_s("%d", &nRange);
 
 
     RandomNumber = GenRandNum(nRange);
     i = GenRandNum(10);
-
+    //정수를 입력한 다음에는 n(3~10)번의 기회를 주고 기회안에 정수를 입력하여 답을 맞추어야 합니다.
     while (1)
     {
         if (i < 3)
@@ -46,7 +46,7 @@ int main(void)
 
     for (int j = 0; j < i; j++)
     {
-        if (Answer > RandomNumber)
+        if (Answer > RandomNumber) //입력한 답보다 값이 낮을때는 down!이 출력되어 다시 맞춰야 합니다.
         {
             printf("Down!\n\n");
             printf("What is the number? : ");
@@ -54,7 +54,7 @@ int main(void)
 
             continue;
         }
-        else if (Answer < RandomNumber)
+        else if (Answer < RandomNumber) //입력한 답보다 값이 높을때는 up!이 출력되어 다시 맞춰야 합니다.
         {
             printf("Up!\n\n");
             printf("What is the number? : ");
@@ -68,13 +68,13 @@ int main(void)
 
     if (Answer == RandomNumber)
     {
-        printf("You enter the correct number~ congratulaion!\n");
+        printf("You enter the correct number~ congratulaion!\n"); //기회안에 맞출경우
 
         return 0;
     }
     else
     {
-        printf(" You enter the wrong number...");
+        printf(" You enter the wrong number..."); //기회안에 못 맞출경우
 
         return 0;
     }
