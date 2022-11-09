edfb36ad780cdc73dc7f0c062a461bb5b3877284
kim2019012659 <als4923@naver.com>
2022-11-09 17:52:30 +0900

    9th week

---
+++ 9th week/Project3/Project3/소스.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str1[20], str2[20], str3[30];
+    char temp[20];
+
+    printf("세 개의 단어 입력 : ");
+    scanf_s("%s%s%s",str1, str2, str3);
+
+    if (strcmp(str1, str2) > 0)
+    {
+        strcpy_s(temp, str1);
+        strcpy_s(str1, str2);
+        strcpy_s(str2, temp);
+    }
+    if (strcmp(str1, str3) > 0)
+    {
+        strcpy_s(temp, str1);
+        strcpy_s(str1, str3);
+        strcpy_s(str3, temp);
+    }
+
+    if (strcmp(str2, str3) > 0)
+    {
+        strcpy_s(temp, str2);
+        strcpy_s(str2, str3);
+        strcpy_s(str3, temp);
+    }
+    printf("%s, %s, %s\n", str1, str2, str3);
+
+    return 0;
+}
