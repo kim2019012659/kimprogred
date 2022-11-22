b960e0abdc0093ad5c09624d2c4f9ec41469ab24
kim2019012659 <als4923@naver.com>
2022-11-22 21:01:21 +0900

    10th week

--- 10th week/Project2/Project2/소스.cpp
+++ 10th week/Project2/Project2/소스.cpp
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct book
+struct book_information
 {
-	char title[80];
-	char author[80];
-	int page;
-	int price;
+    char title[80];
+    char author[80];
+    int page;
+    int price;
 };
 
-int main(void)
-{
-	struct book list[5]{
-		{"πË∆Æ∏«"}
 
-	}
+int main()
+{
+
+
+    struct book_information list[3] = {
+       {"The French Laundry, Per Se", "Thomas Keller", 400 ,95000},
+       {"Batman: Year One", "Frank Miller", 144, 19400},
+       {"Billy Summers", "Stephen King", 608, 16500}
+    };
+
+    for (int i = 0; i < 3; i++)
+        printf("%s\t%s\t%d\t%d\n", list[i].title, list[i].author, list[i].page, list[i].price);
+
+    return 0;
 }
